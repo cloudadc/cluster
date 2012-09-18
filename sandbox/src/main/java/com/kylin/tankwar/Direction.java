b830346827c41bdba0dcc24e56bd564e13f69bbd
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-09-18 20:39:25 +0800

    adjust the original code architecture

---
+++ sandbox/src/main/java/com/kylin/tankwar/Direction.java
@@ -0,0 +1,5 @@
+package com.kylin.tankwar;
+
+public enum Direction {
+	L, LU, U, RU, R, RD, D, LD, STOP
+}
---
+++ sandbox/src/main/java/com/kylin/tankwar/Wall.java
@@ -0,0 +1,27 @@
+package com.kylin.tankwar;
+
+import java.awt.*;
+
+public class Wall {
+	int x, y, w, h;
+	TankClient tc ;
+	
+	public Wall(int x, int y, int w, int h, TankClient tc) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.tc = tc;
+	}
+	
+	public void draw(Graphics g) {
+		Color c = g.getColor();
+		g.setColor(Color.DARK_GRAY);
+		g.fillRect(x, y, w, h);
+		g.setColor(c);
+	}
+	
+	public Rectangle getRect() {
+		return new Rectangle(x, y, w, h);
+	}
+}
