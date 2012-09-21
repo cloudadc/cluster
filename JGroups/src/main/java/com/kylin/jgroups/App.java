3f2b3e74953d337a3f74ef9494add6aab506bea9
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-09-21 15:04:22 +0800

    add jgroups test project

---
+++ JGroups/src/main/java/com/kylin/jgroups/App.java
@@ -0,0 +1,13 @@
+package com.kylin.jgroups;
+
+import org.apache.log4j.Logger;
+
+public class App {
+	
+	private static final Logger logger = Logger.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Start...");
+	}
+
+}
