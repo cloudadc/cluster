9727f890c1d0945b18d5746d059d26f4705d4c30
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-10-04 20:38:08 +0800

    remove legacy redundant code

--- sandbox/src/main/java/com/kylin/tankwar/jgroups/MissileDraw.java
+++
@@ -1,9 +0,0 @@
-package com.kylin.tankwar.jgroups;
-
-import java.io.Serializable;
-
-public class MissileDraw implements Serializable {
-
-	private static final long serialVersionUID = -4600832875517261198L;
-
-}
