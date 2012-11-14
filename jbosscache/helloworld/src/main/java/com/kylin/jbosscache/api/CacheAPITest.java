df631dc5fda6efefe49e4f38f2eb8a7fd90ff669
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-11-14 19:58:50 +0800

    add Jboss cache api testcode

---
+++ jbosscache/helloworld/src/main/java/com/kylin/jbosscache/api/CacheAPITest.java
@@ -0,0 +1,98 @@
+package com.kylin.jbosscache.api;
+
+import org.jboss.cache.Cache;
+import org.jboss.cache.CacheFactory;
+import org.jboss.cache.DefaultCacheFactory;
+import org.jboss.cache.Fqn;
+import org.jboss.cache.Node;
+import org.jboss.cache.config.Configuration;
+
+public class CacheAPITest {
+
+	public static void main(String[] args) {
+
+		CacheAPITest test = new CacheAPITest();
+
+		// test.createCacheUseDefault();
+
+		// test.createCacheUseClasspathXML();
+
+		// test.createCacheUseFilepathXML();
+
+		test.cachingRetrieving();
+		
+		test.cachingRetrievingWithFqn();
+
+	}
+
+	private void cachingRetrievingWithFqn() {
+
+		Cache cache = createCacheUseDefault();
+		Node rootNode = cache.getRoot();
+		Fqn helloWorldFqn = Fqn.fromString("/root/helloWorld");
+		Node helloWorld = rootNode.addChild(helloWorldFqn);
+		
+		cache.put(helloWorldFqn, "isJBossCache", Boolean.TRUE);
+		cache.put(helloWorldFqn, "content", new Content("HelloWorld"));
+		
+		System.out.println(helloWorld.get("isJBossCache"));
+		System.out.println(helloWorld.get("content"));
+		System.out.println(cache.getRoot().hasChild(helloWorldFqn));
+		
+		cache.removeNode(helloWorldFqn);
+	}
+
+	private void cachingRetrieving() {
+
+		Cache cache = createCacheUseDefault();
+		Node rootNode = cache.getRoot();
+		Fqn helloWorldFqn = Fqn.fromString("/root/helloWorld");
+		Node helloWorld = rootNode.addChild(helloWorldFqn);
+		helloWorld.put("isJBossCache", Boolean.TRUE);
+		helloWorld.put("content", new Content("HelloWorld"));
+
+		System.out.println(helloWorld.get("isJBossCache"));
+		System.out.println(helloWorld.get("content"));
+		System.out.println(helloWorld.getFqn());
+		System.out.println(helloWorld.getKeys());
+
+		helloWorld.remove("isJBossCache");
+		helloWorld.remove("content");
+
+		System.out.println(helloWorld.get("isJBossCache"));
+		System.out.println(helloWorld.get("content"));
+
+		rootNode.removeChild(helloWorldFqn);
+
+	}
+
+	private Cache createCacheUseFilepathXML() {
+
+		CacheFactory factory = new DefaultCacheFactory();
+		Cache cache = factory.createCache(
+				"/opt/configurations/cache-configuration.xml", false);
+		Configuration config = cache.getConfiguration();
+		config.setClusterName("Cluster Test");
+		cache.create();
+		cache.start();
+
+		return cache;
+	}
+
+	private Cache createCacheUseClasspathXML() {
+
+		CacheFactory factory = new DefaultCacheFactory();
+		Cache cache = factory.createCache("total-replication.xml");
+
+		return cache;
+	}
+
+	private Cache createCacheUseDefault() {
+
+		CacheFactory factory = new DefaultCacheFactory();
+		Cache cache = factory.createCache();
+
+		return cache;
+	}
+
+}
