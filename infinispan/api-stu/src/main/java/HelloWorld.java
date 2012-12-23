a96e0dcfe957aac71bcf8a2ebc9eccad4ed665b8
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-12-23 20:57:52 +0800

    add hotrod java client code

---
+++ infinispan/api-stu/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author kylin
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Infinispan is a in-memory No-SQL Data Grid");
+	}
+
+}
