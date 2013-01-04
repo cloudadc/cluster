7baae6f79da0da372203ef6a6c0d4aaccbb97949
Kylin <songzhiqi_1214@yahoo.com.cn>
2013-01-04 17:03:35 +0800

    add JGroups performance test code

---
+++ JGroups/src/test/java/com/kylin/tankwar/jgroups/demo/DateFormatTest.java
@@ -0,0 +1,13 @@
+package com.kylin.tankwar.jgroups.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateFormatTest {
+
+	public static void main(String[] args) {
+
+		System.out.println();
+	}
+
+}
