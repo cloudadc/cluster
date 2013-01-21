d7fb7ff5491641ee53b334748770ae74c898a114
Kylin <songzhiqi_1214@yahoo.com.cn>
2013-01-21 21:17:12 +0800

    add console input to helloworld code

---
+++ jbosscache/helloworld/src/main/java/com/kylin/jbosscache/demo/JBossCacheInputConsole.java
@@ -0,0 +1,12 @@
+package com.kylin.jbosscache.demo;
+
+import com.customized.tools.cli.TreeInputConsole;
+import com.customized.tools.cli.TreeNode;
+
+public class JBossCacheInputConsole extends TreeInputConsole {
+
+	public JBossCacheInputConsole(String name, TreeNode currentNode) {
+		super(name, currentNode);
+	}
+
+}
