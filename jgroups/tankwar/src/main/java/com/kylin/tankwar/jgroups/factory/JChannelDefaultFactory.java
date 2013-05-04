7cd9adb4560b6e53c8f0ced6a78c7cfe6ffa17ea
Kylin <kylinsoong.1214@gmail.com>
2013-05-04 13:11:20 +0800

    add JMX management function to jgroups tankwar demo

--- jgroups/tankwar/src/main/java/com/kylin/tankwar/jgroups/factory/JChannelDefaultFactory.java
+++ jgroups/tankwar/src/main/java/com/kylin/tankwar/jgroups/factory/JChannelDefaultFactory.java
@@ -1,9 +1,13 @@
 package com.kylin.tankwar.jgroups.factory;
 
+import javax.management.MBeanServer;
+
 import org.apache.log4j.Logger;
 import org.jgroups.Channel;
 import org.jgroups.JChannel;
 import org.jgroups.ReceiverAdapter;
+import org.jgroups.jmx.JmxConfigurator;
+import org.jgroups.util.Util;
 
 import com.kylin.tankwar.jgroups.TankWarCommunicationException;
 
@@ -48,6 +52,13 @@ public class JChannelDefaultFactory implements ChannelFactory {
 			channel.setReceiver(reciever);
 			channel.setDiscardOwnMessages(true);
 			channel.connect(cluster);
+			
+			MBeanServer server = Util.getMBeanServer();
+	        if(server == null){
+	        	throw new Exception("No MBeanServers found;" + "\nTankWar needs to be run with an MBeanServer present");
+	        }
+	        JmxConfigurator.registerChannel((JChannel)channel, server, "jgroups-tankwar", channel.getClusterName(), true);
+			
 			return channel ;
 		} catch (Exception e) {
 			throw new TankWarCommunicationException("connect to " + cluster + " error", e);
